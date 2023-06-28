392a39774d2c10343b40fc72396f72539de89e40
drakenlander <javierignacio0212@gmail.com>
2023-06-28 09:33:18 -0600

    Added Excel file row selection

--- pythonProject1/main.py
+++ pythonProject1/main.py
@@ -7,7 +7,6 @@ import careers
 import charts
 
 # TODO: Add Excel file visualization with filtering
-# TODO: Add Excel file row selection
 # TODO: Add Excel charting algorithms
 # TODO: Add Excel file conversion (?)
 # TODO: Add table Year Filter
@@ -137,9 +136,10 @@ class App(tk.Tk):
                     file = open(dt_string + ".csv", "w", encoding="utf-8")
                     file.write(sel)
 
-        def select():
+        def select(trv):
             curItems = trv.selection()
             copy = ""
+            print("select:", curItems)
 
             for i in curItems:
                 row = trv.item(i).get("values")
@@ -155,6 +155,7 @@ class App(tk.Tk):
             return copy
 
         def copySelection(selection):
+            print("copy:", selection)
             self.clipboard_clear()
             self.clipboard_append(selection)
 
@@ -181,7 +182,8 @@ class App(tk.Tk):
         trv.heading("10", text="Fecha de Emisión del Título")
         trv.heading("11", text="Plan de Estudios")
 
-        trv.bind('<ButtonRelease-1>', lambda e: copySelection(select()))
+        trv.bind('<ButtonRelease-1>', lambda e: copySelection(select(trv)))
+        exTrv.bind('<ButtonRelease-1>', lambda e: copySelection(select(exTrv)))
 
         menubar = tk.Menu(self)
         pg_menu = tk.Menu(menubar, tearoff=False)
