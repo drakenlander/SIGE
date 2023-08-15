423f45a6f7b31ffee99f68e181262755c4f20788
drakenlander <javierignacio0212@gmail.com>
2023-08-15 10:33:42 -0600

    Phone Number Selection and Formatting

--- emailSelection/main.py
+++ emailSelection/main.py
@@ -32,5 +32,49 @@ for i in careerArr:
     emailDict[i] += emailArr
     emailArr.clear()
 
-print(len(emailDict))
-print(emailDict)
+# print(len(emailDict))
+# print(emailDict)
+
+selectPhoneNumbers = '''SELECT "CELULAR", "TELEFONO_1", "TELEFONO_2"
+                        FROM public.excelforconversion
+                        WHERE "PROGRAMA" = 'LICENCIATURA EN INGENIERIA INDUSTRIAL'
+                        OR "PROGRAMA" = 'LICENCIATURA EN ADMINISTRACION DE EMPRESAS';'''
+cursor.execute(selectPhoneNumbers)
+
+phoneNumberArr = []
+
+for i in cursor:
+    if not ((i[0] is None or i[0] == 0) and (i[1] is None or i[1] == 0) and (i[2] is None or i[2] == 0)):
+        phoneNumberArr.append(i)
+
+formattedArr = [[0] * 3] * len(phoneNumberArr)
+file = open("phnum.csv", "w")
+
+for i in range(len(formattedArr)):
+    for j in range(3):
+        elem = phoneNumberArr[i][j]
+
+        if elem is None:
+            elem = 0
+
+        if elem != 0:
+            jStr = str(elem)
+
+            if len(jStr) < 7:
+                elem = 0
+
+            if len(jStr) == 7:
+                jStr = "8" + jStr
+                elem = int(jStr)
+
+        formattedArr[i][j] = elem
+
+        if str(formattedArr[i][j])[0] == "2":
+            formattedArr[i][j] = 0
+
+    if not (formattedArr[i][0] + formattedArr[i][1] + formattedArr[i][2] == 0):
+        iStr = str(formattedArr[i])[1:-1]
+
+        file.write(iStr + "\n")
+
+file.close()
